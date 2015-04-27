5e42e295fd5e51aef82595e3ab665f77a2f754f3
tabersays <taber.fitzgerald@build.com>
2015-04-27 09:49:07 -0700

    enables cookies capturing

--- click/click.js
+++ click/click.js
@@ -2,26 +2,36 @@
  * Created by taber.fitzgerald on 4/16/2015.
  * This is the base template for js
  */
-function redirect(path){
-    Cookies.set('test', 'Exactly as named');
+var time;
+var start;
+var end;
+function redirect() {
+    end = new Date();
+    var count = Cookies.get('count');
+    Cookies.set('time' + count, end.getTime() - start.getTime());
+    Cookies.set('count', parseInt(Cookies.get('count'), 10) + 1);
+
 
 }
-$(document).ready(function(){
-    redirect('hello');
-    $('.click').on('click', function(e){
-        if($(e.target).hasClass('selected')){
+
+$(document).ready(function () {
+    start = new Date();
+    if (!Cookies.get('count'))
+        Cookies.set('count', 0);
+    $('.click').on('click', function (e) {
+        if ($(e.target).hasClass('selected')) {
             $(e.target).siblings('div').addClass('hide');
-            if($(e.target).hasClass('c1')){
+            if ($(e.target).hasClass('c1')) {
                 $('.one').addClass('hide').removeClass('selected');
                 $('.two').addClass('hide');
                 $('.c1').removeClass('selected');
                 $('.c2').removeClass('selected');
             }
-            if($(e.target).hasClass('c2')){
+            if ($(e.target).hasClass('c2')) {
                 $('.two').addClass('hide').removeClass('selected');
                 $('.c2').removeClass('selected');
             }
-            if($(e.target).hasClass('c0')){
+            if ($(e.target).hasClass('c0')) {
                 $('.one').addClass('hide').removeClass('selected');
                 $('.two').addClass('hide').removeClass('selected');
                 $('.c1').removeClass('selected');
@@ -29,19 +39,20 @@ $(document).ready(function(){
             }
             $(e.target).siblings().removeClass('selected');
             $(e.target).removeClass('selected');
+            redirect();
         }
-        else{
-            if($(e.target).hasClass('c1')){
+        else {
+            if ($(e.target).hasClass('c1')) {
                 $('.one').addClass('hide').removeClass('selected');
                 $('.two').addClass('hide');
                 $('.c1').removeClass('selected');
                 $('.c2').removeClass('selected');
             }
-            if($(e.target).hasClass('c2')){
+            if ($(e.target).hasClass('c2')) {
                 $('.two').addClass('hide').removeClass('selected');
                 $('.c2').removeClass('selected');
             }
-            if($(e.target).hasClass('c0')){
+            if ($(e.target).hasClass('c0')) {
                 $('.one').addClass('hide').removeClass('selected');
                 $('.two').addClass('hide').removeClass('selected');
                 $('.c1').removeClass('selected');
@@ -50,6 +61,7 @@ $(document).ready(function(){
             $(e.target).siblings().removeClass('selected');
             $(e.target).siblings().removeClass('hide');
             $(e.target).addClass('selected');
+            redirect();
         }
     })
 });
